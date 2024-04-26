baa1f5f3f5e5c4c8929efa28a65c49bd11230a4d
Münker, Simon <muenker@uni-trier.de>
2024-04-26 08:52:16 +0200

    Initial commit

---
+++ Session-2.py
@@ -0,0 +1,48 @@
+import typing
+import re
+
+import collections
+
+from nltk.corpus import stopwords
+
+corpus: str = open('material/voltair-candide.txt').read()
+
+class Sentence:
+
+    def __init__(self, text: str) -> None:
+        self.text = text
+
+    @property
+    def tokens(self) -> typing.List[str]:
+        return self.text.lower().split()
+
+
+    @property
+    def hashtags(self) -> typing.List[str]:
+        return re.sub(r'#\S+', '<hashtag>', self.text)
+
+class Corpus:
+
+    def __init__(self, corpus: str) -> None:
+        self.corpus = corpus
+        self.sentences: typing.List[Sentence] = [
+            Sentence(sent) for sent in corpus.split('.')
+        ]
+    
+    @property
+    def tokens(self) -> typing.List[typing.List[str]]:
+        return [sent.tokens for sent in self.sentences]
+    
+    def calc_freq(self) -> collections.Counter: 
+        _stopwords = stopwords.words('english')
+
+        return collections.Counter([
+            tok 
+            for sent in self.sentences 
+            for tok in sent.tokens
+            if tok not in _stopwords
+        ])
+
+mySent = Corpus(corpus)
+# print(mySent.text)
+print(mySent.calc_freq().most_common(40))
