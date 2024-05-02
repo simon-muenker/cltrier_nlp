0b812e80a1959ad79b7db252c036b450472b6ada
simon-muenker <simon.muenker@googlemail.com>
2024-05-02 08:59:04 +0200

    fix attribute naming

--- cltrier_nlp/corpus/__init__.py
+++ cltrier_nlp/corpus/__init__.py
@@ -75,7 +75,7 @@ class Corpus(pydantic.BaseModel):
             sentences=(
                 subset := [sent for sent in self.sentences if sent.language == language]
             ),
-            raw=" ".join([sent.content for sent in subset]),
+            raw=" ".join([sent.raw for sent in subset]),
         )
 
     def generate_ngrams(self, n: int) -> typing.List[typing.Tuple[str, ...]]:
